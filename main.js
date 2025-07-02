9527b61df20a0c42d9d2d7c40156952506c79cc0
Nicos Weg <softwarengineer010@gmail.com>
2025-07-02 17:57:49 +0500

    Except the heart btn, works gut

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
 const container = document.querySelector('.container');
-const clear = document.querySelector('.button-3');
+const input = document.getElementById('size-grid');
+const set = document.getElementById('set-grid-size')
+const hexelop = document.querySelector('.button-3');
 const slider = document.getElementById('clear-range');
 const color = document.getElementById('color');
 const cursor = document.querySelector('.cursor');
 const heart = document.querySelector('.button-2');
 const ball = document.querySelector('.ball');
 const ballButton = document.querySelector('.button-4');
+const secretMes = document.querySelector('.secret-message');
 
-container.style.display = 'flex';
-container.style.flexWrap = 'wrap';
+
 
 let isTouched = false;
 color.value = '#000000';
 let currentValue;
 let isColoured = false;
 let isClicked = false;
+let gridSize = 16;
+
+
 
 color.addEventListener('change', (event) => {
   currentValue = event.target.value;
@@ -22,7 +27,28 @@ color.addEventListener('change', (event) => {
 });
 
 
+set.addEventListener('click', () => {
+  if(input.value > 2 && input.value <= 100) {
+     gridSize = parseInt(input.value);
+      input.value = '';
+      clearGrid();
+  }else {
+    alert('Input is invalid');
+  }
+ 
+});
+
+hexelop.addEventListener('click', () => {
+ currentValue = 'white';
+ isColoured = true;
+ color.value = '#ffffff';
+});
+
+
 function createGrid(int) {
+  container.style.gridTemplateColumns = `repeat(${int}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${int}, 1fr)`;
+
   for (let i = 0; i<int*int; i++) {
     const gridItem = document.createElement('div');
     gridItem.addEventListener('mousedown', () => {
@@ -31,30 +57,40 @@ function createGrid(int) {
     gridItem.addEventListener('mouseup', () => {
       isTouched = false;
     });
-    gridItem.addEventListener('mouseover', (event) => {
-      if(isTouched) {
-        if(isColoured) {
-             event.target.style.backgroundColor = currentValue;
-        }
-        else {
-          event.target.style.backgroundColor = 'black';
-        }
-        
-      }
-    });
-    slider.addEventListener('change', () => {
-      gridItem.style.backgroundColor = 'white';
-    })
+    gridItem.addEventListener('mouseover', setColor);
+    slider.addEventListener('change', clearGrid);
     gridItem.classList.add('grid-item');
     container.appendChild(gridItem);
   }
 };
 
-createGrid(16);
+function setColor() {
+  if(isTouched) {
+    if(isColoured) {
+      this.style.backgroundColor = currentValue;
+    }
+    else {
+      this.style.backgroundColor = 'black';
+    }
+  }
+}
+
+createGrid(gridSize);
+
+function clearGrid() {
+  container.innerHTML = '';
+  createGrid(gridSize);
+}
+ballButton.addEventListener('click', () => {
+  if (!isClicked) {
+    moveCursor();
+    alert('TO STOP RELOAD THE PAGE (づ ◕‿◕ )づ');
+  }
+})
 
 
 /* <----------Move the ball to where pointer clicked ------------> */
-/*
+
 const moveCursor = () => {
   window.addEventListener('mousemove', (e) => {
   let x = e.clientX;
@@ -64,15 +100,12 @@ const moveCursor = () => {
 })
 }
 
-window.addEventListener('load', moveCursor);
 
 window.addEventListener('click', (e) => {
 
   ball.style.transform = `translateY(${e.clientY - 15}px)`;
   ball.style.transform += `translateX(${e.clientX - 15}px)`;
-}, false);*/
-
-
+}, false); 
 
 
 
